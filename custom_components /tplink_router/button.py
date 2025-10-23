ac678eee3b6b5266acf79399c988623dd998a8a1
potat0man <67843546+potat0man@users.noreply.github.com>
2025-10-23 11:40:06 +0300

    Update button.py

--- custom_components /tplink_router/button.py
+++ custom_components /tplink_router/button.py
@@ -1,73 +1,46 @@
-"""Component providing support for TPLinkRouter button entities."""
-from __future__ import annotations
-
-from collections.abc import Callable
-from dataclasses import dataclass
-from typing import Any
-from homeassistant.const import EntityCategory
-from homeassistant.components.button import (
-    ButtonDeviceClass,
-    ButtonEntity,
-    ButtonEntityDescription,
-)
-from homeassistant.config_entries import ConfigEntry
+from homeassistant.components.button import ButtonEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.config_entries import ConfigEntry
+
 from .const import DOMAIN
-from .coordinator import TPLinkRouterCoordinator
-
-
-@dataclass
-class TPLinkRouterButtonEntityDescriptionMixin:
-    method: Callable[[TPLinkRouterCoordinator], Any]
-
-
-@dataclass
-class TPLinkButtonEntityDescription(
-    ButtonEntityDescription, TPLinkRouterButtonEntityDescriptionMixin
-):
-    """A class that describes button entities for the host."""
-
-
-BUTTON_TYPES = (
-    TPLinkButtonEntityDescription(
-        key="reboot",
-        name="Reboot",
-        device_class=ButtonDeviceClass.RESTART,
-        entity_category=EntityCategory.CONFIG,
-        method=lambda coordinator: coordinator.reboot(),
-    ),
-)
-
 
 async def async_setup_entry(
-        hass: HomeAssistant,
-        entry: ConfigEntry,
-        async_add_entities: AddEntitiesCallback,
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
-    coordinator = hass.data[DOMAIN][entry.entry_id]
+    """Set up buttons."""
+    client = hass.data[DOMAIN][config_entry.entry_id]["client"]
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
 
-    buttons = []
+    async_add_entities([RebootButton(coordinator, client)])
 
-    for description in BUTTON_TYPES:
-        buttons.append(TPLinkRouterButtonEntity(coordinator, description))
-    async_add_entities(buttons, False)
+class RebootButton(ButtonEntity):
+    """Representation of a reboot button."""
 
+    def __init__(self, coordinator, client):
+        self._client = client
+        self._coordinator = coordinator
+        self._attr_unique_id = f"archer_mr200_reboot"
+        device_info = coordinator.data.get("device_info", {})
+        device_name = device_info.get("model", "").lower().replace(" ", "_")
+        self.entity_id = f"button.{device_name}_reboot"
+        self._attr_name = "Reboot"
 
-class TPLinkRouterButtonEntity(CoordinatorEntity[TPLinkRouterCoordinator], ButtonEntity):
-    entity_description: TPLinkButtonEntityDescription
-
-    def __init__(
-            self,
-            coordinator: TPLinkRouterCoordinator,
-            description: TPLinkButtonEntityDescription,
-    ) -> None:
-        super().__init__(coordinator)
-
-        self._attr_device_info = coordinator.device_info
-        self._attr_unique_id = f"{coordinator.unique_id}_{DOMAIN}_{description.key}"
-        self.entity_description = description
+    @property
+    def device_info(self):
+        """Return device info."""
+        device_info = self._coordinator.data.get("device_info", {})
+        return {
+            "identifiers": {(DOMAIN, "archer_mr200")},
+            "name": "TP-Link Archer MR200",
+            "manufacturer": device_info.get("manufacturer"),
+            "model": device_info.get("model"),
+            "hw_version": device_info.get("hw_version"),
+            "sw_version": device_info.get("sw_version"),
+        }
 
     async def async_press(self) -> None:
-        await self.entity_description.method(self.coordinator)
+        """Handle the button press."""
+        await self.hass.async_add_executor_job(self._client.reboot)
